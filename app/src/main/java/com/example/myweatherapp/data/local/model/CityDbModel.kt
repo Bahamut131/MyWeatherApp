342f41ccca3734028388fc8952f29b057c6526c2
Aleksey <bahamutadam@gmail.com>
2024-10-16 00:10:01 +0300

    Creating data base and interface DAO

---
+++ app/src/main/java/com/example/myweatherapp/data/local/model/CityDbModel.kt
@@ -0,0 +1,12 @@
+package com.example.myweatherapp.data.local.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity("cities")
+data class CityDbModel(
+    @PrimaryKey
+    val id : Int,
+    val name : String,
+    val country : String
+)
